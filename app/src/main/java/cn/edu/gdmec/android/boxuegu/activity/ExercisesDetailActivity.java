c251582f0b8e22f2e7fc5632d6a1203e9afe2fc5
LBWNB <862496956@qq.com>
2017-12-26 14:55:45 +0800

    test4

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/ExercisesDetailActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/ExercisesDetailActivity.java
@@ -77,32 +77,121 @@ public class ExercisesDetailActivity extends AppCompatActivity {
         });
         adapter =new ExercisesDetailAdapter(ExercisesDetailActivity.this,
                 new ExercisesDetailAdapter.OnSelectListener() {
-                     @Override
-                    public void onSelectD(int position, ImageView iv_a,ImageView iv_b,ImageView iv_c,ImageView iv_d){
-    if (ebl.get(position).answer !=4){
-        ebl.get(position).select=4;
-    }else {
-        ebl.get(position).select=0;
-    }
-    switch (ebl.get(position).answer){
-        case 1:
-            iv_a.setImageResource(R.drawable.exercises_right_icon);
-            iv_c.setImageResource(R.drawable.exercises_error_icon);
-            break;
-        case 2:
-            iv_d.setImageResource(R.drawable.exercises_error_icon);
-            iv_b.setImageResource(R.drawable.exercises_right_icon);
-            break;
-        case 3:
-            iv_d.setImageResource(R.drawable.exercises_error_icon);
-            iv_c.setImageResource(R.drawable.exercises_right_icon);
-            break;
-        case 4:
-            iv_d.setImageResource(R.drawable.exercises_right_icon);
-            break;
-    }
+                    @Override
+                    public void onSelectD(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+                        if (ebl.get(position).answer != 4) {
+                            ebl.get(position).select = 4;
+                        } else {
+                            ebl.get(position).select = 0;
+                        }
+                        switch (ebl.get(position).answer) {
+                            case 1:
+                                iv_a.setImageResource(R.drawable.exercises_right_icon);
+                                iv_c.setImageResource(R.drawable.exercises_error_icon);
+                                break;
+                            case 2:
+                                iv_d.setImageResource(R.drawable.exercises_error_icon);
+                                iv_b.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 3:
+                                iv_d.setImageResource(R.drawable.exercises_error_icon);
+                                iv_c.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 4:
+                                iv_d.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                        }
+                        AnalysisUtils.setABCDEnable(false,iv_a,iv_b,iv_c,iv_d);
+
                     }
-                }
+                    @Override
+                    public void onSelectC(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+                        if (ebl.get(position).answer != 3) {
+                            ebl.get(position).select = 3;
+                        } else {
+                            ebl.get(position).select = 0;
+                        }
+                        switch (ebl.get(position).answer) {
+                            case 1:
+                                iv_a.setImageResource(R.drawable.exercises_right_icon);
+                                iv_c.setImageResource(R.drawable.exercises_error_icon);
+                                break;
+                            case 2:
+                                iv_b.setImageResource(R.drawable.exercises_right_icon);
+                                iv_c.setImageResource(R.drawable.exercises_error_icon);
+                                break;
+                            case 3:
+
+                                iv_c.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 4:
+                                iv_c.setImageResource(R.drawable.exercises_error_icon);
+                                iv_d.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                        }
+                        AnalysisUtils.setABCDEnable(false,iv_a,iv_b,iv_c,iv_d);
+
+                    }
+                    @Override
+                    public void onSelectB(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+                        if (ebl.get(position).answer != 2) {
+                            ebl.get(position).select = 2;
+                        } else {
+                            ebl.get(position).select = 0;
+                        }
+                        switch (ebl.get(position).answer) {
+                            case 1:
+                                iv_a.setImageResource(R.drawable.exercises_right_icon);
+                                iv_b.setImageResource(R.drawable.exercises_error_icon);
+                                break;
+                            case 2:
+
+                                iv_b.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 3:
+                                iv_b.setImageResource(R.drawable.exercises_error_icon);
+                                iv_c.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 4:
+                                iv_b.setImageResource(R.drawable.exercises_error_icon);
+                                iv_d.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                        }
+                        AnalysisUtils.setABCDEnable(false,iv_a,iv_b,iv_c,iv_d);
+
+                    }
+
+                    @Override
+                    public void onSelectA(int position, ImageView iv_a, ImageView iv_b, ImageView iv_c, ImageView iv_d) {
+                        if (ebl.get(position).answer != 1) {
+                            ebl.get(position).select = 1;
+                        } else {
+                            ebl.get(position).select = 0;
+                        }
+                        switch (ebl.get(position).answer) {
+                            case 1:
+                                iv_a.setImageResource(R.drawable.exercises_right_icon);
+
+                                break;
+                            case 2:
+                                iv_a.setImageResource(R.drawable.exercises_error_icon);
+                                iv_b.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 3:
+                                iv_a.setImageResource(R.drawable.exercises_error_icon);
+                                iv_c.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                            case 4:
+                                iv_a.setImageResource(R.drawable.exercises_error_icon);
+                                iv_d.setImageResource(R.drawable.exercises_right_icon);
+                                break;
+                        }
+                        AnalysisUtils.setABCDEnable(false, iv_a, iv_b, iv_c, iv_d);
+                    }
+                    });
+                    adapter.setData(ebl);
+        lv_list.setAdapter(adapter);
+
 
     }
 
--- app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesAdapter.java
@@ -1,8 +1,83 @@
 package cn.edu.gdmec.android.boxuegu.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.activity.ExercisesDetailActivity;
+import cn.edu.gdmec.android.boxuegu.bean.ExercisesBean;
+
 /**
  * Created by student on 17/12/25.
  */
 
-public class ExercisesAdapter {
+public class ExercisesAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<ExercisesBean> ebl;
+    public ExercisesAdapter(Context context){
+        this.mContext = context;
+    }
+    public void setData(List<ExercisesBean> ebl){
+        this.ebl = ebl;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return ebl == null ? 0 :ebl.size();
+    }
+
+    @Override
+    public ExercisesBean getItem(int position) {
+        return ebl ==null? null : ebl.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder vh;
+        if (convertView == null){
+            vh =new ViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.exercises_list_item,null);
+            vh.title = (TextView)convertView.findViewById(R.id.tv_title);
+            vh.content = (TextView)convertView.findViewById(R.id.tv_content);
+            vh.order = (TextView)convertView.findViewById(R.id.tv_order);
+            convertView.setTag(vh);
+        }else{
+            vh = (ViewHolder)convertView.getTag();
+        }
+        final ExercisesBean bean = getItem(position);
+        if (bean!=null){
+            vh.order.setText(position+1+"");
+            vh.title.setText(bean.title);
+            vh.content.setText(bean.content);
+            vh.order.setBackgroundResource(bean.background);
+        }
+        convertView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (bean == null)
+                    return;
+                Intent intent = new Intent(mContext, ExercisesDetailActivity.class);
+                intent.putExtra("id",bean.id);
+                mContext.startActivity(intent);
+            }
+        });
+        return convertView;
+    }
+    class ViewHolder{
+        public TextView title,content;
+        public TextView order;
+    }
 }
