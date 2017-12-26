a4c76cbb8b710b330ac5c4d7a01adfd782fec0be
qiulifeng <1170537031@qq.com>
2017-12-26 15:05:06 +0800

    test

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesDetailAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesDetailAdapter.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.bean.ExercisesBean;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
