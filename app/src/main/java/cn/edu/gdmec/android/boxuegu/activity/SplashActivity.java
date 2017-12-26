e273f4a99d717c1def63a4f593b6093728487d9e
LBWNB <862496956@qq.com>
2017-12-26 11:55:41 +0800

    test3

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -1,15 +1,46 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+private TextView tv_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+    private void init(){
+        tv_version=(TextView) findViewById(R.id.tv_version);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("v"+info.versionName);
+        }catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            tv_version.setText("v");
+        }
+        Timer timer = new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);
+
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/boxuegu/view/ExerciseView.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.gdmec.android.boxuegu.view;
-
-/**
- * Created by student on 17/12/25.
- */
-
-public class ExerciseView {
-}
